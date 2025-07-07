bd63abbbba751304a7171142f8ca0c315d4fa20c
Shahzada Shoaib <shoaibkhankhan0345@gmail.com>
2025-07-07 17:25:49 +0500

    Add Swagger API documentation setup
    Integrated @nestjs/swagger and configured SwaggerModule in main.ts to provide API documentation at the /docs endpoint. Updated dependencies in package.json to include @nestjs/swagger.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 import { ConfigService } from './common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const configService = app.get(ConfigService)
+
+  // Set global API prefix
+  app.setGlobalPrefix('api')
+
+  const config = new DocumentBuilder()
+    .setTitle('Tasks API')
+    .setDescription('API for managing tasks')
+    .setVersion('1.0')
+    .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, documentFactory)
 
   await app.listen(configService.getPort())
 }
