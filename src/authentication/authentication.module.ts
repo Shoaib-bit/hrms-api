dff2aeab9f939a0db936cfc8142910b8bb38d815
Shahzada Shoaib <shoaibkhankhan0345@gmail.com>
2025-07-10 10:57:15 +0500

    Update authentication module and permission exports
    Added CommonModule import to AuthenticationModule and updated its exports. Also re-exported permission.controller in the permission module. Updated .env.example to use 'postgres' as the database user.

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common'
+import { CommonModule } from 'src/common'
 import { AuthController } from './auth'
 import { AuthenticationService } from './authentication.service'
 import { PermissionController } from './permission'
 import { RoleController } from './role'
 
 @Module({
+  imports: [CommonModule],
   controllers: [RoleController, PermissionController, AuthController],
   providers: [AuthenticationService],
-  exports: [AuthenticationService]
+  exports: []
 })
 export class AuthenticationModule {}
